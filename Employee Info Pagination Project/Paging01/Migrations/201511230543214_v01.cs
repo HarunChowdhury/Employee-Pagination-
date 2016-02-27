9dfec7e4c060e8cb3e5efda067f86740effd6d4f
harunchowdhury <trishamomo1@gmail.com>
2016-02-27 23:22:45 +0600

    Version 01

---
+++ Employee Info Pagination Project/Paging01/Migrations/201511230543214_v01.cs
@@ -0,0 +1,28 @@
+namespace Paging01.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class v01 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Employees",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Gender = c.String(),
+                        Email = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Employees");
+        }
+    }
+}
---
+++ Employee Info Pagination Project/Paging01/Models/SampleDBContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Services.Description;
+
+namespace Paging01.Models
+{
+    public class SampleDBContext:DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
